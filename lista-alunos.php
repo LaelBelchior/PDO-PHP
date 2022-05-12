9ab473486e424b3837c67225f4011fd1c6a42197
Lael Belchior <laelbelchior@gmail.com>
2022-05-11 21:11:35 -0300

    Fazendo um array com instancias da class Student com os dados do banco de dados

--- lista-alunos.php
+++ lista-alunos.php
@@ -1,11 +1,22 @@
 <?php
 
+use Alura\Pdo\Domain\Model\Student;
+
 require_once 'vendor/autoload.php';
 
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
 $statement = $pdo->query('SELECT * FROM students;');
-$studentList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$studentList = [];
+
+foreach($studentDataList as $student){
+    $studentList[] = new Student(
+        $student['id'],
+        $student['name'],
+        new DateTimeImmutable($student['birth_date'])
+    );
+}
 
 var_dump($studentList);
